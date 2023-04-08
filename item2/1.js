d091df27b84a21e07c6ee81ad56eb543260afee4
lxc482113948 <482113948@qq.com>
2023-04-08 14:19:33 +0800

    修改

---
+++ item2/1.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: LiuXueChao
+ * @Date: 2023-04-08 14:17:08
+ * @LastEditors: your name
+ * @LastEditTime: 2023-04-08 14:17:18
+ * @Description: file content
+ */
+(function () {
+    var item2 = {};
+    item2.one = function () {
+        console.log('item2/1.js');
+    };
+})();
